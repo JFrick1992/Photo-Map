5a998e4c171af6fdcdeef45aca44598215f6d303
Jacob Frick <jacobfrick@Jacobs-MacBook-Air-2.local>
2018-10-21 21:16:32 -0700

    2nd user story complete

--- Photo Map/FullImageViewController.swift
+++ Photo Map/FullImageViewController.swift
@@ -21,15 +21,12 @@ class FullImageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        dismiss(animated: true, completion: {() -> Void in })
+       
+        
     }
-    */
+    
 
 }
--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import MapKit
 
-class PhotoMapViewController: UIViewController {
-
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var image = UIImage()
+    
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,11 +26,26 @@ class PhotoMapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func onTapMapButton(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        self.present(vc, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage]{
+            self.image =  image as! UIImage
+        }
+        dismiss(animated: true, completion: {() -> Void in
+            self.performSegue(withIdentifier: "tagSegue", sender: nil)
+            
+        })
     
-    // MARK: - Navigation
+        
+    }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
